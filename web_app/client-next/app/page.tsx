e8158dd848a712e1d2683c91b358545af7b27b69
Martin Klokočík <mklokocik@gmail.com>
2025-04-22 06:54:43 +0200

    [Home] redirect to dashboard on load

--- web_app/client-next/app/page.tsx
+++ web_app/client-next/app/page.tsx
@@ -1,11 +1,19 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/dashboard')
+  }, [router])
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>HELLO WORLD</h1>
-        <br></br>
+        <h1>Redirecting to dashboard...</h1>
       </main>
     </div>
   )
