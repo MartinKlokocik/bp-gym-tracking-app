55940a07ccf84f5ca7f2ae8060fe6d20b88abaa4
Martin Klokočík <mklokocik@gmail.com>
2025-05-08 00:28:17 +0200

    [BuildFixes] 3

--- web_app/client-next/middleware.ts
+++ web_app/client-next/middleware.ts
@@ -1,28 +1,35 @@
+import { NextResponse } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
 
-export default withAuth({
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/auth/login', // Path to your custom login page
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(_req) {
+    return NextResponse.next()
   },
-  callbacks: {
-    authorized: ({ req, token }) => {
-      // Allow requests to /login and /signup without a token
-      const { pathname } = req.nextUrl
-      if (
-        pathname.startsWith('/auth/login') ||
-        pathname.startsWith('/auth/signup')
-      ) {
-        return true
-      }
-      // Otherwise, require the user to be authenticated
-      return !!token
+  {
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+      signIn: '/auth/login', // Path to your custom login page
     },
-  },
-})
+    callbacks: {
+      authorized: ({ req, token }) => {
+        // Allow requests to /login and /signup without a token
+        const { pathname } = req.nextUrl
+        if (
+          pathname.startsWith('/auth/login') ||
+          pathname.startsWith('/auth/signup')
+        ) {
+          return true
+        }
+        // Otherwise, require the user to be authenticated
+        return !!token
+      },
+    },
+  }
+)
 
 // Specify which routes should be checked by the middleware.
-// Here, we protect everything except favicon.ico
+// Here, we protect everything except favicon.ico and public routes
 export const config = {
-  matcher: ['/((?!favicon.ico).*)'],
+  matcher: ['/((?!favicon.ico|_next/static|_next/image|api/auth).*)'],
 }
